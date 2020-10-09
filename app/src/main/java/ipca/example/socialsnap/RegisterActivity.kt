61b687543ad3565f7ddc40c5519b3d16ed69f575
Lourenço Gomes <leto.lg@gmail.com>
2020-10-09 14:19:33 +0100

    first commit

---
+++ app/src/main/java/ipca/example/socialsnap/RegisterActivity.kt
@@ -0,0 +1,52 @@
+package ipca.example.socialsnap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_register_activity.*
+
+class RegisterActivity : AppCompatActivity() {
+
+
+    companion object {
+        val TAG = "LoginActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_activity)
+
+        auth = Firebase.auth
+
+        buttonRegister.setOnClickListener {
+            auth.createUserWithEmailAndPassword(
+                editTextName.text.toString(),
+                editTextPassword.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
+
+    }
+}
---
+++ app/src/main/java/ipca/example/socialsnap/SplashScreenActivity.kt
@@ -0,0 +1,39 @@
+package ipca.example.socialsnap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+
+        currentUser?.let {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }?:run{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+/*
+        Handler(Looper.getMainLooper()).postDelayed({
+
+        }, 1000)
+*/
+    }
+}
